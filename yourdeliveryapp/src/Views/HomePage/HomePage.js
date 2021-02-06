10a49f65f9b3ed07b75ccd0e0b9a7a0707ced256
shikhar gupta <shikhar.gupta.che15@itbhu.ac.in>
2021-02-07 01:38:28 +0530

    new project

---
+++ yourdeliveryapp/src/Views/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="App-header">
+       <h1>
+       YOUR DELIVERY By Shikhar Gupta and Anubhav chittarth
+       </h1>
+       
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
